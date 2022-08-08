45523c5da68a4e756d163bd2870e665a80847acc
Jeff Nfor <jeffnfor@gmail.com>
2022-08-07 22:52:33 -0400

    initial update

--- terraform.tfvars
+++ terraform.tfvars
@@ -6,11 +6,11 @@ app_tags = "Example"
 
 application_name = "Example-App"
 
-vpc_id = "vpc-ee2325320"
+vpc_id = "vpc-0c41889a2c45c25f5"
 
-ec2_subnets = "subnet-d1c325ab"
+ec2_subnets = "subnet-0fd8f85701e9c7109"
 
-elb_subnets = ["subnet-d1c325ab","subnet-d1c565ab"]
+elb_subnets = ["subnet-0fd8f85701e9c7109", "subnet-02d4bb7cc0dac091c"]
 
 instance_type = "t3.micro"
 
@@ -18,5 +18,5 @@ disk_size = "20"
 
 keypair = "example"
 
-sshrestrict="12.34.56.78/32"
+sshrestrict="172.31.64.0/20"
 alarm_sns_topic = "arn:aws:sns:us-east-1:123456788900:TOPIC"
--- vars.tf
+++ vars.tf
@@ -1,23 +1,19 @@
-variable "region" {}
-
-variable "domain_name" {}
-
-variable "app_tags" {}
-
-variable "application_name" {}
-
-variable "vpc_id" {}
-
-variable "ec2_subnets" {}
-
-variable "elb_subnets" {}
-
-variable "instance_type" {}
-
-variable "disk_size" {}
-
-variable "keypair" {}
-
-variable "sshrestrict" {}
-
-variable "alarm_sns_topic" {}
+variable "elasticapp" {
+  default = "myapplication"
+}
+variable "beanstalkappenv" {
+  default = "myenvironment"
+}
+variable "solution_stack_name" {
+  default = "64bit Amazon Linux 2 v5.5.4 running Node.js 12"
+}
+variable "tier" {
+  default = "WebServer"
+}
+ 
+variable "vpc_id" {
+    default = "vpc-0c41889a2c45c25f5"
+}
+variable "public_subnets" {
+    default = ["subnet-0fd8f85701e9c7109", "subnet-02d4bb7cc0dac091c"]
+}
